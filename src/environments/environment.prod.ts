25ace90fe501e0667b0d7e8664564804e0450a84
RodartHub <rodrigovalencia95@hotmail.com>
2023-07-02 16:59:45 -0500

    App de tareas hecha en ionic

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyDZSPJtfSEXPcBrogK0KDaKXVS2lHUTCkg",
+    authDomain: "test-db-odoo.firebaseapp.com",
+    projectId: "test-db-odoo",
+    storageBucket: "test-db-odoo.appspot.com",
+    messagingSenderId: "638411588042",
+    appId: "1:638411588042:web:3355d8081776bc2da38f33"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyDZSPJtfSEXPcBrogK0KDaKXVS2lHUTCkg",
+    authDomain: "test-db-odoo.firebaseapp.com",
+    projectId: "test-db-odoo",
+    storageBucket: "test-db-odoo.appspot.com",
+    messagingSenderId: "638411588042",
+    appId: "1:638411588042:web:3355d8081776bc2da38f33"
+  }
 };
 
 /*
